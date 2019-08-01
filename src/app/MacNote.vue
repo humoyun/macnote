7fabfc2a310782d3474362dc84da9f360cdd954c
Khumoyun Akhmedov <humoyun@3i.ai>
2019-08-02 08:32:28 +0900

    Main Layout updates

--- src/app/MacNote.vue
+++ src/app/MacNote.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="mac-note">
-    <div class="mc-open-btn">
-      <mc-button @click="openModal()">Open</mc-button>
+  <div id="macnote-app">
+    <div class="mc-header">
+      <span>Macnote App</span>
     </div>
-
-    <modal v-model="visible"></modal>
+    <div class="mc-container">
+      <div class="mc-app-header"></div>
+      <div class="mc-app-body">
+        <div class="mc-body-folders"></div>
+        <div class="mc-body-notes"></div>
+        <div class="mc-body-content"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,16 +39,74 @@ export default {
 </script>
 
 <style>
-#mac-note {
+#macnote-app {
   width: 100%;
   height: 100vh;
-  background-color: rgb(7, 121, 141);
+  background-color: rgb(7, 181, 194);
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 }
 
-.mc-open-btn {
-  width: 200px;
+.mc-header {
+  padding: 20px;
+  font-size: 32px;
+  margin-top: -30px;
 }
+
+.mc-container {
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  min-width: 600px;
+  height: 80vh;
+  background-color: rgb(250, 249, 249);
+  border-radius: 4px;
+  box-shadow: 0 3px 6px 3px rgba(0,0,0,0.2);
+  transition: all 0.3s;
+}
+
+.mc-container:hover {
+  box-shadow: 0 5px 10px 5px rgba(0,0,0,0.2);
+}
+
+.mc-app-header {
+  display: flex;
+  flex: 1;
+  border-bottom: 1px solid rgb(230, 225, 225);
+}
+
+.mc-app-body {
+  display: flex;
+  flex: 13;
+}
+
+.mc-body-folders {
+  display: flex;
+  flex: 2;
+  border-right: 1px solid rgb(230, 230, 236);
+}
+
+.mc-body-notes {
+  display: flex;
+  flex: 3;
+  border-right: 1px solid rgb(230, 230, 236);
+}
+
+.mc-body-content {
+  display: flex;
+  flex: 5;
+}
+
+@media only screen and (max-width: 1000px) {
+  .mc-body-folders {
+    flex: 0;
+  }
+
+  .mc-body-notes {
+    flex: 2;
+  }
+}
+
 </style>
