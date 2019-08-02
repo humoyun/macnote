8b9fbfd32b47ebcaac0f8f9969a303adc7b70942
Khumoyun Akhmedov <humoyun@3i.ai>
2019-08-02 12:15:48 +0900

    Sass support & Favicon update

--- src/app/MacNote.vue
+++ src/app/MacNote.vue
@@ -2,6 +2,7 @@
   <div id="macnote-app">
     <div class="mc-header">
       <span>Macnote App</span>
+      <span>Currently under development</span>
     </div>
     <div class="mc-container">
       <div class="mc-app-header"></div>
@@ -33,65 +34,74 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #macnote-app {
   width: 100%;
   height: 100vh;
-  background-color: rgb(7, 181, 194);
+  background-color: rgb(5, 120, 133);
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-}
 
-.mc-header {
-  padding: 20px;
-  font-size: 32px;
-  margin-top: -30px;
-}
+  .mc-header {
+    padding: 20px;
+    margin-top: -30px;
+  }
 
-.mc-container {
-  display: flex;
-  flex-direction: column;
-  width: 80vw;
-  min-width: 600px;
-  height: 80vh;
-  background-color: rgb(250, 249, 249);
-  border-radius: 4px;
-  box-shadow: 0 3px 6px 3px rgba(0,0,0,0.2);
-  transition: all 0.3s;
-}
+  .mc-header > span {
+    display: block;
+    color: $grey-color;
+  }
 
-.mc-container:hover {
-  box-shadow: 0 5px 10px 5px rgba(0,0,0,0.2);
-}
+  .mc-header > span:nth-child(1) {
+    font-size: 32px;
+    color: white;
+  }
 
-.mc-app-header {
-  display: flex;
-  flex: 1;
-  border-bottom: 1px solid rgb(230, 225, 225);
-}
+  .mc-container {
+    display: flex;
+    flex-direction: column;
+    width: 80vw;
+    min-width: 600px;
+    height: 80vh;
+    background-color: rgb(250, 249, 249);
+    border-radius: 4px;
+    box-shadow: 0 3px 6px 3px rgba(0,0,0,0.2);
+    transition: all 0.3s;
 
-.mc-app-body {
-  display: flex;
-  flex: 13;
-}
+    .mc-app-header {
+      display: flex;
+      flex: 1;
+      border-bottom: 1px solid rgb(230, 225, 225);
+    }
 
-.mc-body-folders {
-  display: flex;
-  flex: 2;
-  border-right: 1px solid rgb(230, 230, 236);
-}
+    .mc-app-body {
+      display: flex;
+      flex: 13;
 
-.mc-body-notes {
-  display: flex;
-  flex: 3;
-  border-right: 1px solid rgb(230, 230, 236);
-}
+      .mc-body-folders {
+        display: flex;
+        flex: 2;
+        border-right: 1px solid rgb(230, 230, 236);
+      }
 
-.mc-body-content {
-  display: flex;
-  flex: 5;
+      .mc-body-notes {
+        display: flex;
+        flex: 3;
+        border-right: 1px solid rgb(230, 230, 236);
+      }
+
+      .mc-body-content {
+        display: flex;
+        flex: 5;
+      }
+    }
+  }
+
+  .mc-container:hover {
+    box-shadow: 0 5px 10px 5px rgba(0,0,0,0.2);
+  }
 }
 
 @media only screen and (max-width: 1000px) {
@@ -103,5 +113,4 @@ export default {
     flex: 2;
   }
 }
-
 </style>
